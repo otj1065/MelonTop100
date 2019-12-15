a237c89fb2fa493d52122d256e7b28f5d6da0530
otj1065 <44268429+otj1065@users.noreply.github.com>
2019-12-15 15:39:22 +0900

    Update melonTOP100.py

--- melonTOP100.py
+++ melonTOP100.py
@@ -18,21 +18,16 @@ targetSite='https://www.melon.com/chart/index.htm'
 # https://developers.whatismybrowser.com/useragents/explore/layout_engine_name/trident
 header = {'User-agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko'}
 melonrqRetry=rq.get(targetSite, headers=header)
-# print(melonrqRetry)
 
 # 사이트 html 읽기
 melonht=melonrqRetry.text
-# print(melonht)
 
 # beautifulSoup 활용
 melonsp=bs(melonht,'html.parser')
-# print(melonsp)
 
 # findAll 혹은 select 활용하여 필요한 정보 수집
 # class="checkEllipsis"
 artists=melonsp.findAll('span',{'class' :'checkEllipsis'})
-# 조금 더 까다로움 가공할 때에! - html을 잘 관찰하여서 다른 부분에서 따옴!
-# print(artists)
 # ellipsis rank01
 titles=melonsp.findAll('div',{'class' :'ellipsis rank01'})
 
@@ -67,6 +62,5 @@ file=open('melonTOP100.txt','r',-1,'UTF-8')
 lines=file.readlines()
 for line in lines:
      print(line.strip())
-     #line,end='' 도 사용가능
 file.close()
 print('melonTOP100.txt 파일에 읽기 완료')
